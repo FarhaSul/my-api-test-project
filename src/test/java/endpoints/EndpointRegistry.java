4aa38d4c4f66fae20ca88c11b95cdab7374ed0d1
FarhaSul <faaha26@gmail.com>
2026-04-06 15:22:49 -0400

    Update EndpointRegistry.java

--- src/test/java/endpoints/EndpointRegistry.java
+++ src/test/java/endpoints/EndpointRegistry.java
@@ -12,7 +12,11 @@ public class EndpointRegistry {
         endpoints.put("user", new EndpointDefinition( "/user/{username}", List.of("GET", "PUT", "DELETE") ));
         endpoints.put("login", new EndpointDefinition( "/user/login", List.of("GET") ));
         endpoints.put("pet", new EndpointDefinition("/pet", List.of("GET", "PUT", "POST") ));
-        endpoints.put("petId", new EndpointDefinition(/pet/{petId}, List.of("GET", "PUT", "POST") ))
+        endpoints.put("petId", new EndpointDefinition(/pet/{petId}, List.of("GET", "PUT", "POST") ));
+        endpoints.put("store" new EndpointDefinition( /store/order/{orderId}, List.of("GET", "DELETE")));
+        endpoints.put("store" new EndpointDefinition( /store/order , List.of("POST")));
+        endpoints.put("store" new EndpointDefinition( /store/inventory , List.of("GET")));
+
     }
 
     public static EndpointDefinition get(String name) {
